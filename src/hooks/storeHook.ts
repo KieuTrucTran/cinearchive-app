da1b3f8d1f2f2dd714755dae05025e69ed151f76
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2025-03-26 18:03:40 +0100

    Setup Redux toolkit for state management

---
+++ src/hooks/storeHook.ts
@@ -0,0 +1,8 @@
+// Custom hooks for interacting with the Redux store
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+
+import { AppDispatch, RootState } from "../store";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
