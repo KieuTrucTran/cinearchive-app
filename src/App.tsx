de283345786e940d1e25541c7b364c622d135cf1
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2025-03-28 15:29:21 +0100

    Displaying data with useSelector

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { useAppDispatch, useAppSelector } from "./hooks/storeHook";
 import { getMovies } from "./features/movies/movieSlice";
+import MovieCard from "./components/MovieCard/MovieCard";
 
 function App() {
-  const { darkTheme } = useAppSelector((state) => state);
+  const { darkTheme, movies } = useAppSelector((state) => state);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -18,6 +19,23 @@ function App() {
       {/*dark mode styling*/}
       <div className="dark:bg-blue-900 dark:text-white min-h-screen px-4 lg:px-12 pb-20">
         <Header />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10">
+          {movies.data &&
+            movies.data.results.map((movie) => {
+              const { id, title, overview, poster_path } = movie;
+              return (
+                <MovieCard
+                  key={id}
+                  title={title}
+                  overview={overview}
+                  poster_path={
+                    "https://image.tmdb.org/t/p/original" + poster_path
+                  }
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,34 @@
+/* Display individual movie details */
+import { FC } from "react";
+
+interface MovieCardProps {
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+const MovieCard: FC<MovieCardProps> = (props) => {
+  const { poster_path, title, overview } = props;
+  return (
+    <div
+      className="max-w-sm bg-white mx-auto rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 hover:-translate-y-4 
+    duration-200 hover:animate-pulse cursor-pointer"
+    >
+      <img
+        src={poster_path}
+        alt={title}
+        className="rounded-t-lg h-80 w-full object-cover"
+      ></img>
+      <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+        <p className=">mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {overview.slice(0, 300)}...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
