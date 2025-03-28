0a3ed185be8b925741e1a10cbb6e0f468b00d2ed
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2025-03-28 17:52:31 +0100

    Implement search feature and add SearchBox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header/Header";
 import { useAppDispatch, useAppSelector } from "./hooks/storeHook";
 import { getMovies } from "./features/movies/movieSlice";
 import MovieCard from "./components/MovieCard/MovieCard";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
   const { darkTheme, movies } = useAppSelector((state) => state);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getMovies());
   }, [dispatch]);
 
+  const searchMovies = movies.data?.results.filter((movie) => {
+    if (!searchTerm.length) return movie;
+    if (!movie.title) return;
+    return movie.title.toLowerCase().includes(searchTerm);
+  });
+
   return (
     <div className={darkTheme ? "dark" : ""}>
       {/*dark mode styling*/}
       <div className="dark:bg-blue-900 dark:text-white min-h-screen px-4 lg:px-12 pb-20">
         <Header />
+        <div className="mb-12 flex items-center justify-between">
+          <SearchBox setSearchTerm={setSearchTerm} />
+        </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10">
-          {movies.data &&
-            movies.data.results.map((movie) => {
+          {searchMovies &&
+            searchMovies.map((movie) => {
               const { id, title, overview, poster_path } = movie;
               return (
                 <MovieCard
